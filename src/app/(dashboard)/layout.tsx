a187b804d82fdd96bd54dcce67cc7d836011b468
Akshay Kumar <akshay@zuperior.com>
2025-10-19 11:40:57 +0530

    07:dashboard-navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
-import DashboardSidebar from "@/modules/dashboard/ui/components/dashboard-navbar";
+import DashboardNavbar from "@/modules/dashboard/ui/components/dashboard-navbar";
+import DashboardSidebar from "@/modules/dashboard/ui/components/dashboard-sidebar";
 import { ReactNode } from "react";
 
 interface Props {
@@ -11,6 +12,7 @@ const DashboardLayout = ({ children }: Props) => {
     <SidebarProvider>
       <DashboardSidebar />
       <main className="flex flex-col h-screen w-screen bg-muted">
+        <DashboardNavbar />
         {children}
       </main>
     </SidebarProvider>
--- src/modules/dashboard/ui/components/dashboard-navbar.tsx
+++ src/modules/dashboard/ui/components/dashboard-navbar.tsx
@@ -1,107 +1,55 @@
 "use client";
 
-import { Separator } from "@/components/ui/separator";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar";
-import { cn } from "@/lib/utils";
-import { BotIcon, StarIcon, VideoIcon } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import DashboardUserButton from "./dashboard-user-button";
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/components/ui/sidebar";
+import { PanelLeftCloseIcon, PanelLeftIcon, SearchIcon } from "lucide-react";
+import DashboardCommand from "./dashboard-command";
+import { useEffect, useState } from "react";
 
-const firstSection = [
-  { icon: VideoIcon, label: "Meetings", href: "/meetings" },
-  { icon: BotIcon, label: "Agents", href: "/agents" },
-];
+const DashboardNavbar = () => {
+  const [commandOpen, setCommandOpen] = useState(false);
+  const { state, toggleSidebar, isMobile } = useSidebar();
 
-const secondSection = [{ icon: StarIcon, label: "Upgrade", href: "/upgrade" }];
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setCommandOpen((open) => !open);
+      }
+    };
 
-const DashboardSidebar = () => {
-  const pathname = usePathname();
+    document.addEventListener("keydown", down);
+    return () => {
+      document.removeEventListener("keydown", down);
+    };
+  }, []);
 
   return (
-    <Sidebar>
-      <SidebarHeader className="text-sidebar-accent-foreground">
-        <Link href="/" className="flex items-center gap-2 px-2">
-          <Image src="/logo.svg" alt="" height={36} width={36} />
-          <p className="text-2xl font-semibold">Meet.AI</p>
-        </Link>
-      </SidebarHeader>
-      <div className="px-4 py-2">
-        <Separator className="opacity-10 text-[#5D6B68]" />
-      </div>
-      <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {firstSection.map((item) => (
-                <SidebarMenuItem key={item.href}>
-                  <SidebarMenuButton
-                    asChild
-                    className={cn(
-                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
-                      pathname === item.href &&
-                        "bg-linear-to-r/oklch border-[#5D6B68]/10"
-                    )}
-                    isActive={pathname === item.href}
-                  >
-                    <Link href={item.href}>
-                      <item.icon className="size-5" />
-                      <span className="text-sm font-medium tracking-tight">
-                        {item.label}
-                      </span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-        <div className="px-4 py-2">
-          <Separator className="opacity-10 text-[#5D6B68]" />
-        </div>
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {secondSection.map((item) => (
-                <SidebarMenuItem key={item.href}>
-                  <SidebarMenuButton
-                    asChild
-                    className={cn(
-                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
-                      pathname === item.href &&
-                        "bg-linear-to-r/oklch border-[#5D6B68]/10"
-                    )}
-                    isActive={pathname === item.href}
-                  >
-                    <Link href={item.href}>
-                      <item.icon className="size-5" />
-                      <span className="text-sm font-medium tracking-tight">
-                        {item.label}
-                      </span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
-      <SidebarFooter className="text-white">
-        <DashboardUserButton />
-      </SidebarFooter>
-    </Sidebar>
+    <>
+      <DashboardCommand open={commandOpen} setOpen={setCommandOpen} />
+      <nav className="flex px-4 gap-x-2 items-center py-3 border-b bg-background">
+        <Button className="size-9" variant="outline" onClick={toggleSidebar}>
+          {state === "collapsed" || isMobile ? (
+            <PanelLeftIcon className="size-4" />
+          ) : (
+            <PanelLeftCloseIcon className="size-4" />
+          )}
+        </Button>
+        <Button
+          variant="outline"
+          className="h-9 w-[240px] justify-start font-normal text-muted-foreground hover:text-muted-foreground"
+          size="sm"
+          onClick={() => setCommandOpen((open) => !open)}
+        >
+          <SearchIcon />
+          Search
+          <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground">
+            <span className="text-xs">&#8984;</span>
+          </kbd>
+        </Button>
+      </nav>
+    </>
   );
 };
 
-export default DashboardSidebar;
+export default DashboardNavbar;
